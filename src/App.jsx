0ff2ac2215c5b794644c326251c6d7728b9d3231
michellepachecoweb <michellepachecoweb@gmail.com>
2024-04-11 19:40:20 -0300

    feat: Adicionar componentes iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import './styles.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return ( 
+    <div> 
+      <Header />
+      <Post 
+        author="Michelle Pacheco" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae laudantium libero blanditiis nobis maiores veniam, necessitatibus suscipit exercitationem molestiae. Delectus natus cumque minus deleniti sequi voluptas magnam accusamus officia enim!"
+      />
+      <Post 
+        author="Dolores Fernandes da Silva" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          teste de contar {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
-
-export default App
+ 
