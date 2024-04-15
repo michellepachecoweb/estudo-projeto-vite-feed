5e6fd5e0dbe7ac0ef353dfcfe767f970c0ae1f9d
michellepachecoweb <michellepachecoweb@gmail.com>
2024-04-15 20:11:46 -0300

    feat: Concluir estrutura da Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Header } from './components/Header'
 import { Post } from './components/Post'
-
+import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import './globals.css'
 
@@ -11,8 +11,9 @@ export function App() {
     <div> 
       <Header />
       <div className={styles.wrapper}>
-        <aside> SIDEBAR</aside>
+        <Sidebar />
         <main>
+
           <Post 
             author="Michelle Pacheco" 
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae laudantium libero blanditiis nobis maiores veniam, necessitatibus suscipit exercitationem molestiae. Delectus natus cumque minus deleniti sequi voluptas magnam accusamus officia enim!"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,22 @@
-export function
+import styles from './Sidebar.module.css'
+
+export function Sidebar () {
+    return (
+        <aside className={styles.sidebar}>            
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1587620931276-d97f425f62b9?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <div className={styles.profile}>
+                <strong>Michelle Pacheco</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href='#'>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+
+    ) 
+}
