c09c2a29f8e1438222d3dea5cb1ce609f36eb492
michellepachecoweb <michellepachecoweb@gmail.com>
2024-04-17 11:05:25 -0300

    feat: Continuar post e adicionar componente de comentário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -12,9 +12,15 @@ export function Post(props) {
                 </div>
             </div>
 
-            <time dateTime='2022-05-11 10:57:10'>Publicado há 1h</time>
+            <time title="11 de maio de 2024'" dateTime='2024-05-11 10:57:10'>Publicado há 1h</time>
 
         </header> 
+        <div className={styles.content}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p>Ad dolorem omnis nesciunt, soluta consectetur cumque tenetur facilis. </p>
+            <p>Delectus modi ducimus aperiam? Odio laboriosam aliquid, nostrum officia officiis inventore? Accusantium, eveniet?</p>
+            <a href='#'> {''} #teste</a>
+        </div>
     </article>
  )
 }
